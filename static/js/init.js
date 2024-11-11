e108bd3a5739faccc725515b0d885165aac748bd
biyk <biyk@inbox.ru>
2024-11-11 21:16:16 +0400

    Полировка таба инициатива

--- static/js/init.js
+++ static/js/init.js
@@ -6,7 +6,7 @@ let charactersData = []; // Хранение загруженных данных
 // Функция для загрузки данных из JSON-файла
 function loadInitiativeData() {
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "/static/data.json", true);
+    xhr.open("GET", "/api/config/init", true);
     xhr.onload = function () {
         if (xhr.status === 200) {
             const data = JSON.parse(xhr.responseText);
@@ -57,6 +57,30 @@ function nextTurn() {
     sendInit(); // Отправка данных на сервер после перехода хода
 }
 
+
+// Переход к предыдущему персонажу
+function prevTurn() {
+    // Упорядочиваем персонажей по инициативе (по убыванию)
+    const characters = charactersData.sort((a, b) => parseFloat(b.init) - parseFloat(a.init));
+
+    // Находим текущего персонажа и предыдущего по инициативе
+    let currentIndex = characters.findIndex(character => parseFloat(character.init) === parseFloat(currentCharacterIndex));
+
+    // Если текущий персонаж первый, нужно вернуться к последнему персонажу и уменьшить раунд
+    if (currentIndex === 0) {
+        currentCharacterIndex = characters[characters.length - 1].init;
+        currentRound = Math.max(1, currentRound - 1); // Уменьшаем раунд, но не меньше 1
+    } else {
+        // В противном случае, переходим к предыдущему персонажу
+        currentCharacterIndex = characters[currentIndex - 1].init;
+    }
+
+    // Обновляем информацию о текущем и следующем персонаже
+    displayCharacters();
+    sendInit(); // Отправка данных на сервер после перехода хода назад
+}
+
+
 // Обновление информации о текущем и следующем персонаже
 function displayCurrentAndNextTurn() {
     const characters = charactersData.sort((a, b) => parseFloat(b.init) - parseFloat(a.init));
@@ -70,6 +94,7 @@ function displayCurrentAndNextTurn() {
 }
 
 // Модифицированная функция для отображения строк персонажей с подсветкой текущего
+// Модифицированная функция для отображения строк персонажей с учетом НПС
 function displayCharacters() {
     const container = document.getElementById('characters-container');
     container.innerHTML = '';
@@ -84,12 +109,17 @@ function displayCharacters() {
             row.classList.add('current-turn');
         }
 
+        if (character.npc==='true') {
+            row.classList.add('character-npc');
+        }
+
         row.innerHTML = `
-            <span>Инициатива: <input type="text" value="${character.init}" onchange="updateCharacterInit(${index}, this.value)" /></span>
+            <span>Init: <input type="text" value="${character.init}" onchange="updateCharacterInit(${index}, this.value)" /></span>
             <span>Имя: ${character.name}</span>
             <span>КД: ${character.cd}</span>
             <span>HP: <span class="hp-now"><input type="text" value="${character.hp_now}" onchange="updateCharacterHp(${index}, this.value)" /></span> / ${character.hp_max}</span>
             <label>Сюрприз: <input type="checkbox" ${character.surprise === "true" ? "checked" : ""} /></label>
+            <label>НПС: <input type="checkbox" ${character.npc === "true" ? "checked" : ""} onchange="updateCharacterNpc(${index}, this.checked)" /></label>
             <button onclick="deleteCharacter(${index})">-</button>
             <button onclick="healCharacter(${index})">+</button>
         `;
@@ -97,6 +127,7 @@ function displayCharacters() {
     });
     displayCurrentAndNextTurn();
 }
+
 
 // Функция обновления инициативы персонажа
 function updateCharacterInit(index, value) {
@@ -112,11 +143,19 @@ function updateCharacterHp(index, value) {
     sendInit(); // Отправка данных на сервер при обновлении здоровья
 }
 
+function updateCharacterNpc(index, isChecked) {
+    charactersData[index].npc = isChecked ? "true" : "false"; // Устанавливаем значение "true" или "false" в зависимости от состояния чекбокса
+    displayCharacters();
+    sendInit(); // Отправка данных на сервер при обновлении признака НПС
+}
+
 // Функция сброса инициативы
 function resetInitiative() {
     charactersData.forEach((character) => {
         character.init = '';
     });
+    currentCharacterIndex = 99;
+    currentRound = 0;
     displayCharacters();
     sendInit(); // Отправка данных на сервер после сброса инициативы
 }
@@ -154,6 +193,8 @@ function addCharacter() {
     const hpNow = document.getElementById('new-hp-now').value;
     const hpMax = document.getElementById('new-hp-max').value;
     const surprise = document.getElementById('new-surprise').checked;
+    const npc = document.getElementById('new-npc').checked; // Получаем значение НПС
+    const exp = document.getElementById('new-experience').value; // Получаем значение НПС
 
     // Проверяем уникальность инициативы
     while (!isUniqueInitiative(init, charactersData)) {
@@ -166,7 +207,9 @@ function addCharacter() {
         cd: cd,
         hp_now: hpNow,
         hp_max: hpMax,
-        surprise: surprise ? "true" : "false"
+        exp: exp,
+        surprise: surprise ? "true" : "false",
+        npc: npc ? "true" : "false" // Добавляем признак НПС
     };
 
     charactersData.push(newCharacter);
