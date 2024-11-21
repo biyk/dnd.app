3a56e4db41b3659c994a045debc23b2bb1a60956
biyk <biyk@inbox.ru>
2024-11-21 12:15:21 +0400

    Мелкая оптимизания

--- static/js/npc.js
+++ static/js/npc.js
@@ -4,6 +4,7 @@ export class NpcManager {
     constructor() {
         const el = id => document.getElementById(id);
         this.apiUrl = '/api/data/location';
+        // Ссылки на элементы
         this.searchNpcInput = el('npc-template-search');
         this.searchNpcResults = el('template-npc-search-results');
         this.npcCd = el('npc-kd');
@@ -13,59 +14,56 @@ export class NpcManager {
         this.monsters = new Map();
         this.init();
     }
+
     init() {
         this.searchNpcInput.addEventListener('input', debounce(() => this.searchNpc(), 300));
+        document.getElementById('npc-form').addEventListener('submit', this.handleFormSubmit.bind(this));
+    }
 
-
-        document.getElementById('npc-form').addEventListener('submit', function (event) {
-            event.preventDefault(); // Предотвращаем стандартную отправку формы
-
-            const formData = new FormData(this);
-
-            fetch('/api/data/npc/add', {
+    async handleFormSubmit(event) {
+        event.preventDefault(); // Предотвращаем стандартную отправку формы
+        try {
+            const response = await fetch('/api/data/npc/add', {
                 method: 'POST',
-                body: formData,
-            })
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Ошибка при отправке формы');
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    console.info(data.message);
-                })
-                .catch(error => {
-                    alert('Произошла ошибка: ' + error.message);
-                });
-        });
+                body: new FormData(event.target)
+            });
+            if (!response.ok) throw new Error('Ошибка при отправке формы');
+            const { message } = await response.json();
+            console.info(message);
+        } catch (error) {
+            alert('Произошла ошибка: ' + error.message);
+        }
     }
 
     async searchNpc() {
         const query = this.searchNpcInput.value.trim();
-        if (!query) return (this.searchNpcResults.innerHTML = '');
+        if (!query) {
+            this.searchNpcResults.innerHTML = '';
+            return;
+        }
         try {
             const response = await fetch(`/api/data/monsters/json?name=${encodeURIComponent(query)}`);
             const data = await response.json();
             this.monsters = new Map(data.map(npc => [npc.id, npc]));
-            this.searchNpcResults.innerHTML = data?.length
+            this.searchNpcResults.innerHTML = data.length
                 ? data.map(npc => `<li data-monster-id="${npc.id}" onclick="window.NpcManager.useTemplate(${npc.id})">${npc.name}</li>`).join('')
                 : '';
         } catch (error) {
             console.error('Ошибка поиска персонажей:', error);
         }
     }
-    async useTemplate(monster) {
-        let npc = this.monsters.get(monster);
-        this.npcCd.value = npc.armor_class;
-        this.npcName.value = npc.name;
-        this.npcHp.value = npc.hit_points;
-        this.npcTemplate.value = npc.id;
-    }
 
+    useTemplate(monsterId) {
+        const npc = this.monsters.get(monsterId);
+        if (npc) {
+            this.npcCd.value = npc.armor_class || '';
+            this.npcName.value = npc.name || '';
+            this.npcHp.value = npc.hit_points || '';
+            this.npcTemplate.value = npc.id || '';
+        } else {
+            console.error(`Шаблон NPC с ID ${monsterId} не найден`);
+        }
+    }
 }
 
 window.NpcManager = new NpcManager();
-
-
-
